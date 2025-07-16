16f6b20dc5194ad0f43b30fd0a9c4ad7033a166c
Jainika Bisen <jainikabisen@Jainikas-MacBook-Pro.local>
2025-07-16 10:31:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { Routes, Route, Navigate} from 'react-router-dom';
+import MarketingPage from './components/MarketingPage';
+import LoginPage from './components/LoginPage';
+import ProductPage from './components/ProductPage';
+
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  return (
+    <Routes>
+      <Route path="/" element={<MarketingPage />} />
+      <Route path="/login" element={<LoginPage onLogin={() => setIsAuthenticated(true)} />} />
+      <Route
+        path="/product"
+        element={isAuthenticated ? <ProductPage onLogout={() => setIsAuthenticated(false)} /> : <Navigate to="/login" />}
+      />
+      <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+    </Routes>
+  );
+};
+
+export default App;
---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import '../LoginPage.css';
+import { useNavigate } from 'react-router-dom';
+
+const LoginPage = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    if (username && password) {
+      onLogin();
+      navigate('/product');
+    } else {
+      alert('Please enter username and password.');
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Login</h1>
+      <input type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default LoginPage;
