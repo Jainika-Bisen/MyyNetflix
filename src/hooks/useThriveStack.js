2bc4d83cd18bc6047038d020cc2fcf5501b63ec6
Jainika Bisen <jainikabisen@Jainikas-MacBook-Pro.local>
2025-07-21 14:51:15 +0530

    TS tracking product page features2

---
+++ src/hooks/useThriveStack.js
@@ -0,0 +1,34 @@
+// src/hooks/useThriveStack.js
+import { useEffect } from "react";
+
+export default function useThriveStack() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://cdn.thrivestack.ai/ts.js";
+    script.async = true;
+    script.setAttribute("data-ts-api-key", "bRroMZSqg+1sv4n3xsQkjjSiA68VrQIP1VSD6aSzIuk=");
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+}
+
+// src/hooks/useThriveStackUser.js
+// import { useEffect } from "react";
+
+// export default function useThriveStackUser(userId, userEmail) {
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       if (window.thriveStack && typeof window.thriveStack.setUser === "function") {
+//         window.thriveStack.setUser(userId, userEmail);
+//         clearInterval(interval);
+//         console.log("ThriveStack user set");
+//       }
+//     }, 300);
+
+//     // Optional: after 5s, stop trying
+//     setTimeout(() => clearInterval(interval), 5000);
+//   }, [userId, userEmail]);
+// }
